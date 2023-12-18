de775c5b12de625cd3b3e996a49cac24268d2365
taham8875 <taha.m8875@gmail.com>
2023-12-18 22:02:45 +0200

    fix: Resolve bug in ChevronButton styling with explicit classNames
    - Address a bug in the ChevronButton component where string interpolation for tailwind CSS classes was not working as expected.
    - Replace string interpolation with explicit classNames for left and right ChevronButton styles.

--- src/components/ChevronButton.tsx
+++ src/components/ChevronButton.tsx
@@ -7,14 +7,13 @@ interface ChevronButtonProps {
 }
 
 const ChevronButton = ({ direction, onClick }: ChevronButtonProps) => {
-  const gradientDirection = direction === "left" ? "to-r" : "to-l";
-  const justifyDirection =
-    direction === "left" ? "justify-start" : "justify-end";
+  const classNameLeft =
+    "absolute top-1/2 bottom-0 left-0 -translate-y-1/2 w-10 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full flex ";
+  const classNameRight =
+    "absolute top-1/2 bottom-0 right-0 -translate-y-1/2 w-10 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end";
 
   return (
-    <div
-      className={`absolute ${direction}-0 top-1/2 -translate-y-1/2 bg-gradient-${gradientDirection} from-white from-50% to-transparent w-24 h-full flex ${justifyDirection}`}
-    >
+    <div className={direction === "left" ? classNameLeft : classNameRight}>
       <YoutubeButton size="icon" variant="ghost" onClick={onClick}>
         {direction === "left" ? <ChevronLeft /> : <ChevronRight />}
       </YoutubeButton>
