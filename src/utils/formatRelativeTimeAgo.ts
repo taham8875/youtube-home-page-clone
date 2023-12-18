c5d1bcca3630676f9f3b873d09b8d173065d2396
taham8875 <taha.m8875@gmail.com>
2023-12-18 22:06:27 +0200

    feat: Add utility function for formatting relative time
    - Introduce a new utility function, `formatTimeAgo`, to convert a given date into a human-readable relative time string.
    - Utilize the Intl.RelativeTimeFormat API to handle the formatting based on the time difference.
    - The function calculates the relative time in seconds, minutes, hours, days, weeks, months, and years, providing a precise representation.

---
+++ src/utils/formatRelativeTimeAgo.ts
@@ -0,0 +1,32 @@
+/**
+ * This utility function takes a date and returns a string representing how long ago that date was.
+ * e.g. "2 hours ago", "3 days ago", "1 year ago"
+ */
+
+const formatter = new Intl.RelativeTimeFormat("en-US", {
+  numeric: "always",
+});
+
+const DIVISIONS: { amount: number; name: Intl.RelativeTimeFormatUnit }[] = [
+  { amount: 60, name: "seconds" },
+  { amount: 60, name: "minutes" },
+  { amount: 24, name: "hours" },
+  { amount: 7, name: "days" },
+  { amount: 4.34524, name: "weeks" },
+  { amount: 12, name: "months" },
+  { amount: Number.POSITIVE_INFINITY, name: "years" },
+];
+
+const formatTimeAgo = (date: Date) => {
+  let duration = (date.getTime() - new Date().getTime()) / 1000;
+
+  for (let i = 0; i < DIVISIONS.length; i++) {
+    const division = DIVISIONS[i];
+    if (Math.abs(duration) < division.amount) {
+      return formatter.format(Math.round(duration), division.name);
+    }
+    duration /= division.amount;
+  }
+};
+
+export default formatTimeAgo;
