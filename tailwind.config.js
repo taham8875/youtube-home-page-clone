ba7009f6c52d2ed6103ce3ab5aef66082112fc59
taham8875 <taha.m8875@gmail.com>
2023-12-20 22:46:51 +0200

    style: Customize scrollbar appearance and colors
    - In `tailwind.config.js`, add a new color `light` to the `secondary` color palette.
    - Introduce two new colors, `hover-2` and `scrollbar`, to the `secondary` color palette.
    - In src/index.css, define styles for customizing the appearance of the webkit browser scrollbar.
    - Set styles to hide the scrollbar by using `bg-transparent` for `::-webkit-scrollbar-thumb`.
    - Apply styles to handle hover interactions for the scrollbar.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,12 @@ export default {
       colors: {
         secondary: {
           DEFAULT: "#f2f2f2",
+          light: "#f2f2f2",
           hover: "#e5e5e5",
+          ["hover-2"]: "#e6e6e6",
           border: "#d3d3d3",
           text: "#606060",
+          scrollbar: "#909090",
           dark: "#0f0f0f",
           ["dark-hover"]: "#030303",
         },
