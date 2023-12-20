d8fb4eab9e70467babc659843fe2900c30b9b5b2
taham8875 <taha.m8875@gmail.com>
2023-12-20 22:54:17 +0200

    feat: Add data files for playlists, sidebar footer links, and subscriptions
    - Added playlists.ts with an array of playlist objects.
    - Added sidebarFooterLinks.ts with an array of sidebar footer link objects.
    - Added subscriptions.ts with an array of subscription objects.

---
+++ src/data/sidebarFooterLinks.ts
@@ -0,0 +1,13 @@
+export const sidebarFooterLinks = [
+  { title: "About", url: "https://www.youtube.com/about/" },
+  { title: "Press", url: "https://www.youtube.com/press" },
+  { title: "Copyright", url: "https://www.youtube.com/copyright" },
+  { title: "Contact us", url: "https://www.youtube.com/contact" },
+  { title: "Creators", url: "https://www.youtube.com/creators" },
+  { title: "Advertise", url: "https://www.youtube.com/advertise" },
+  { title: "Developers", url: "https://developers.google.com/youtube" },
+  { title: "Terms", url: "https://www.youtube.com/t/terms" },
+  { title: "Privacy Policy & Safety", url: "https://www.youtube.com/t/privacy" },
+  { title: "How YouTube works", url: "https://www.youtube.com/howyoutubeworks" },
+  { title: "Test new features", url: "https://www.youtube.com/testnewfeatures" },
+];
